48bb2def95130eb21812cc9c4e851df8a2a30d72
Diva Canto <diva@metaverseink.com>
2010-04-02 08:56:41 -0700

    Fixed license headers.

--- addon-modules/ImageService/ImageServiceModule.cs
+++ addon-modules/ImageService/ImageServiceModule.cs
@@ -1,6 +1,5 @@
 /**
- * Copyright (c) Contributors. All rights reserved.
- * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ * Copyright (c) Crista Lopes (aka Diva). All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, 
  * are permitted provided that the following conditions are met:
