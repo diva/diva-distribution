879f9231ad60fdf11d30f8dc44fc4774a8c1e235
Ryan Hsu <rchsu@uci.edu>
2010-05-13 15:33:51 -0700

    Added ExtensionMethods.

---
+++ addon-modules/Wifi/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenSim.Services.Interfaces;
+
+
+namespace Diva.Wifi
+{
+    public static class ExtensionMethods
+    {
+        public static int GetX(this GridRegion gr)
+        {
+            return gr.RegionLocX / 256;
+        }
+
+        public static int GetY(this GridRegion gr)
+        {
+            return gr.RegionLocY / 256;
+        }
+    }
+}
