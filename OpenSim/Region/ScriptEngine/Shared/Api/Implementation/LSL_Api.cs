29a62abc6d24d5e378f2afb79f68a606be5fc239
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2011-10-10 22:23:42 +0100

    Convert getLastReportedSimFPS() and getLastReportedSimStats() into more idiomatic LastReportedSimFPS and LastReportedSimStats on SimStatsReporter

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -5926,7 +5926,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
         public LSL_Float llGetRegionFPS()
         {
             m_host.AddScriptLPS(1);
-            return World.StatsReporter.getLastReportedSimFPS();
+            return World.StatsReporter.LastReportedSimFPS;
         }
 
 
