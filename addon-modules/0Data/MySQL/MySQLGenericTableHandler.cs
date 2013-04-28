581c32e14fedfbec0486e70fb4a166790979d381
Diva Canto <diva@metaverseink.com>
2013-04-27 21:26:04 -0700

    Deleted methods that are now in core.

--- addon-modules/0Data/MySQL/MySQLGenericTableHandler.cs
+++ addon-modules/0Data/MySQL/MySQLGenericTableHandler.cs
@@ -42,65 +42,6 @@ namespace Diva.Data.MySQL
         public MySQLGenericTableHandler(string connectionString, string realm, string storeName)
             : base(connectionString, realm, storeName) { }
 
-        public virtual long GetCount(string field, string key)
-        {
-            return GetCount(new string[] { field }, new string[] { key });
-        }
-
-        public virtual long GetCount(string[] fields, string[] keys)
-        {
-            if (fields.Length != keys.Length)
-                return 0;
-
-            List<string> terms = new List<string>();
-
-            using (MySqlCommand cmd = new MySqlCommand())
-            {
-                for (int i = 0; i < fields.Length; i++)
-                {
-                    cmd.Parameters.AddWithValue(fields[i], keys[i]);
-                    terms.Add("`" + fields[i] + "` = ?" + fields[i]);
-                }
-
-                string where = String.Join(" and ", terms.ToArray());
-
-                string query = String.Format("select count(*) from {0} where {1}",
-                                             m_Realm, where);
-
-                cmd.CommandText = query;
-
-                Object result = DoQueryScalar(cmd);
-
-                return Convert.ToInt64(result);
-            }
-        }
-
-        public virtual long GetCount(string where)
-        {
-            using (MySqlCommand cmd = new MySqlCommand())
-            {
-                string query = String.Format("select count(*) from {0} where {1}",
-                                             m_Realm, where);
-
-                cmd.CommandText = query;
-
-                object result = DoQueryScalar(cmd);
-
-                return Convert.ToInt64(result);
-            }
-        }
-
-        public virtual object DoQueryScalar(MySqlCommand cmd)
-        {
-            using (MySqlConnection dbcon = new MySqlConnection(m_connectionString))
-            {
-                dbcon.Open();
-                cmd.Connection = dbcon;
-
-                return cmd.ExecuteScalar();
-            }
-        }
-
         new public virtual T[] DoQuery(MySqlCommand cmd)
         {
             return base.DoQuery(cmd);
