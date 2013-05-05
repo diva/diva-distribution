a528b66a6ffdbc81b17252aef3ff901b0aa7b1d6
Diva Canto <diva@metaverseink.com>
2013-05-04 22:02:16 -0700

    More JS utils.

--- WifiPages/scripts/forms.js
+++ WifiPages/scripts/forms.js
@@ -1,3 +1,15 @@
 function showForm(divName, form) {
   $('#' + divName).html(form);
 }
+
+function checkExtension(req_ext) {
+  var ext = document.upload.datafile.value;
+  ext = ext.substring(ext.length-3,ext.length);
+  ext = ext.toLowerCase();
+  if(ext != req_ext.toLowerCase()) {
+    alert('Please select a .' + req_ext+ ' file!');
+    return false; 
+  }
+  else
+    return true; 
+}
