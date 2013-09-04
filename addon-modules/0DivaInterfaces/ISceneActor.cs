aedb1b79981ee0f1cf6dbb630ec8f0a176207518
Diva Canto <diva@metaverseink.com>
2013-09-04 09:07:01 -0700

    Added forgotten files.

---
+++ addon-modules/0DivaInterfaces/ISceneActor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using OpenSim.Framework;
+
+namespace Diva.Interfaces
+{
+    public delegate void SceneAction(IScene s);
+
+    public interface ISceneActor
+    {
+        void ForEachScene(SceneAction d);
+    }
+}
