ab4497fdcdde4190e083689a88d8bf098eb800c9
Diva Canto <diva@metaverseink.com>
2014-12-31 10:28:09 -0800

    Updated assembly version

--- addon-modules/Modules/Properties/AssemblyInfo.cs
+++ addon-modules/Modules/Properties/AssemblyInfo.cs
@@ -30,9 +30,9 @@ using Mono.Addins;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.8.0.*")]
+[assembly: AssemblyVersion("0.8.1.*")]
 
 
-[assembly: Addin("Diva.Modules", "0.1")]
-[assembly: AddinDependency("OpenSim", "0.5")]
+[assembly: Addin("Diva.Modules", OpenSim.VersionInfo.VersionNumber + ".1")]
+[assembly: AddinDependency("OpenSim.Region.Framework", OpenSim.VersionInfo.VersionNumber)]
 
