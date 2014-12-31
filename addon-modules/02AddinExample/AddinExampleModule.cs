17ca8c1a3144b8dd150cd8352c7af4a0ee63c8aa
Diva Canto <diva@metaverseink.com>
2014-12-31 11:51:36 -0800

    Added AddinExample

---
+++ addon-modules/02AddinExample/AddinExampleModule.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using OpenSim.Framework;
+using OpenSim.Framework.Servers;
+using OpenSim.Framework.Servers.HttpServer;
+using OpenSim.Region.Framework;
+using OpenSim.Region.Framework.Interfaces;
+using OpenSim.Region.Framework.Scenes;
+using OpenSim.Server.Base;
+
+using Mono.Addins;
+using log4net;
+using Nini.Config;
+
+// External library that doesn't exist in the OpenSim distribution
+using CsvHelper;
+
+namespace Diva.AddinExample
+{
+    [Extension(Path = "/OpenSim/RegionModules", NodeName = "RegionModule", Id = "AddinsExampleModule")]
+    public class AddinExampleModule : ISharedRegionModule
+    {
+        #region Class and Instance Members
+
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private bool m_Enabled;
+
+        private static string AssemblyDirectory
+        {
+            get
+            {
+                string location = Assembly.GetExecutingAssembly().Location;
+                return Path.GetDirectoryName(location);
+            }
+        }
+
+        #endregion
+
+        #region ISharedRegionModule
+
+        public void Initialise(IConfigSource config)
+        {
+            IConfig cnf = config.Configs["AddinExample"];
+            if (cnf == null)
+                return;
+
+            m_Enabled = cnf.GetBoolean("enabled", m_Enabled);
+            if (m_Enabled)
+            {
+                string pathToCsvFile = Path.Combine(AssemblyDirectory, "Names.csv");
+                string pathToHtmlFile = Path.Combine(AssemblyDirectory, "AddinExample.html");
+                m_log.InfoFormat("[Diva.AddinExample]: AddinExample is on. File is {0}", pathToCsvFile);
+
+                if (!File.Exists(pathToCsvFile))
+                {
+                    using (TextWriter tr = new StreamWriter(pathToCsvFile))
+                    using (var writer = new CsvWriter(tr))
+                    {
+                        writer.WriteField("First Name");
+                        writer.WriteField("Last Name");
+                        writer.NextRecord();
+                    }
+                }
+
+                MainServer.Instance.AddStreamHandler(new FormUploadGetHandler(pathToHtmlFile));
+                MainServer.Instance.AddStreamHandler(new FormUploadPostHandler(pathToCsvFile));
+            }
+        }
+
+        public string Name
+        {
+            get { return "Diva Addin Example"; }
+        }
+
+        public Type ReplaceableInterface
+        {
+            get { return null; }
+        }
+
+        public void PostInitialise() { }
+
+        public void AddRegion(Scene scene) { }
+
+        public void RegionLoaded(Scene scene) { }
+
+        public void RemoveRegion(Scene scene) { }
+
+        public void Close() { }
+
+        #endregion
+
+    }
+
+    public class FormUploadGetHandler : BaseStreamHandler
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private string m_pathToFile;
+
+        public FormUploadGetHandler(string file) :
+            base("GET", "/diva/addinexample")
+        {
+            m_pathToFile = file;
+        }
+
+        protected override byte[] ProcessRequest(string path, Stream requestData,
+                IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        {
+            string result = string.Empty;
+            string resource = GetParam(path);
+
+            string html = string.Empty;
+            using (StreamReader sr = new StreamReader(m_pathToFile))
+                html = sr.ReadToEnd();
+
+            httpResponse.ContentType = "text/html";
+            return Encoding.UTF8.GetBytes(html);
+        }
+    }
+
+    public class FormUploadPostHandler : BaseStreamHandler
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private string m_PathToFile;
+
+        public FormUploadPostHandler(string path) :
+            base("POST", "/diva/addinexample")
+        {
+            m_PathToFile = path;
+        }
+
+        protected override byte[] ProcessRequest(string path, Stream requestData,
+                IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        {
+            // It's a POST, so we need to read the data on the stream, the lines after the blank line
+            StreamReader sr = new StreamReader(requestData);
+            string body = sr.ReadToEnd();
+            sr.Close();
+            body = body.Trim();
+
+            // Here the data on the stream is transformed into a nice dictionary of keys & values
+            Dictionary<string, object> postdata = ServerUtils.ParseQueryString(body);
+
+            string firstname = string.Empty, lastname = string.Empty;
+            if (postdata.ContainsKey("firstname") && !string.IsNullOrEmpty(postdata["firstname"].ToString()))
+                firstname = postdata["firstname"].ToString();
+            if (postdata.ContainsKey("lastname") && !string.IsNullOrEmpty(postdata["lastname"].ToString()))
+                lastname = postdata["lastname"].ToString();
+
+            AddToCSVFile(firstname, lastname);
+
+            string result = "Thanks, your name has been recorded.";
+            httpResponse.ContentType = "text/html";
+            httpResponse.StatusCode = 200;
+
+            return Encoding.UTF8.GetBytes(result);
+
+        }
+
+        private void AddToCSVFile(string first, string last)
+        {
+            using (TextWriter tr = new StreamWriter(m_PathToFile, true))
+            using (var writer = new CsvWriter(tr))
+            {
+                writer.WriteField(first);
+                writer.WriteField(last);
+                writer.NextRecord();
+            }
+        }
+    }
+
+}
---
+++ addon-modules/02AddinExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Mono.Addins;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Diva.AddinExample")]
+[assembly: AssemblyDescription("Example of how to develop OpenSim addins")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Diva.AddinExample.Properties")]
+[assembly: AssemblyCopyright("Diva Canto")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a915261b-15fc-4107-b65d-58ad6640f128")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.8.1.*")]
+
+[assembly: Addin("Diva.AddinExample", OpenSim.VersionInfo.VersionNumber + ".1")]
+[assembly: AddinDependency("OpenSim.Region.Framework", OpenSim.VersionInfo.VersionNumber)]
+[assembly: AddinDescription("Example of how to create OpenSim Addins")]
+[assembly: AddinAuthor("Diva Canto")]
+
+[assembly: ImportAddinAssembly("CsvHelper.dll")]
+[assembly: ImportAddinFile("AddinExample.ini")]
+[assembly: ImportAddinFile("AddinExample.html")]
