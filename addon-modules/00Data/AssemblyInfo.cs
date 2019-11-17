dc97888aea8706a74218109c7abff314b978da21
Diva Canto <diva@metaverseink.com>
2019-11-17 09:54:10 -0800

    Fixed assembly version of Diva.Data

--- addon-modules/00Data/AssemblyInfo.cs
+++ addon-modules/00Data/AssemblyInfo.cs
@@ -29,5 +29,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.7.5.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(OpenSim.VersionInfo.AssemblyVersionNumber)]
